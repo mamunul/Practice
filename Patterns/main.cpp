0fa9cd313ff7dccb072071d1c98f345107ec17e9
Mamunul <mamunul.mazid@gmail.com>
2017-11-11 11:41:30 +0600

    Initial Commit

---
+++ Patterns/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Patterns
+//
+//  Created by Mamunul on 11/11/17.
+//  Copyright © 2017 Mamunul. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
