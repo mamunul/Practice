bc0822337937e7d3a4b09315dbaa470cc5aa3567
Mamunul <mamunul.mazid@gmail.com>
2017-11-14 16:33:25 +0600

    assignment operator behaviour

--- Practice/main.cpp
+++ Practice/main.cpp
@@ -59,14 +59,25 @@ int main(int argc, const char * argv[]) {
 	
 	ExampleA *ea = new ExampleA();
 	
+	e->vir_pub_method2();
+	e2->vir_pub_method2();
+	
+	ExampleB e123 = ExampleB();
+	
+	ExampleB e234 = ExampleA();//values are copied from ExampleA class methods are pointng to ExampleB class -- so ExampleA methods are not visible which is implicit assignment operator behavior, to change it override it.
+	
+	e123.vir_pub_method2();
+	e234.vir_pub_method2();
+
+	
 //	test3(ea);
 	
-	test(e);
-	test(e2);
+//	test(e);
+//	test(e2);
 	
 	
-	test2(*e);
-	test2(*e2);
+//	test2(*e);
+//	test2(*e2);
 //	ex.pub_method();
 
 	
